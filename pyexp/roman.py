546404e18aeac6bf8cacf0cf38c03204d09302de
Cuong Nguyen <cuong.nguyentumanh@gmail.com>
2015-08-23 13:16:23 +0100

    Minor changes in style to comply with pylint.

--- pyexp/roman.py
+++ pyexp/roman.py
@@ -1,10 +1,14 @@
+'''Module to convert to and from Roman numerals.
+Current range supported: 1-3999.
+'''
+
 class OutOfRangeError(ValueError):
     pass
 
 class NotIntegerError(ValueError):
     pass
 
-roman_numeral_map = (('M', 1000),
+ROMAN_NUMERAL_MAP = (('M', 1000),
                      ('CM', 900),
                      ('D', 500),
                      ('CD', 400),
@@ -14,36 +18,36 @@ roman_numeral_map = (('M', 1000),
                      ('XL', 40),
                      ('X', 10),
                      ('IX', 9),
-                     ('V',  5),
+                     ('V', 5),
                      ('IV', 4),
                      ('I', 1))
 
 
-def to_roman(n):
+def to_roman(num_int):
     '''Take an integer between 1 and 3999 and return Roman numeral
     '''
-    if not isinstance(n, int):
+    if not isinstance(num_int, int):
         raise NotIntegerError(
             'Input is not of integer type.')
-    if n >= 4000 or n <= 0:
+    if num_int >= 4000 or num_int <= 0:
         raise OutOfRangeError(
             'Number out of range (must be betwen 1 and 3999 inclusive).')
     result = ''
-    for numeral, integer in roman_numeral_map:
-        while n >= integer:
+    for numeral, integer in ROMAN_NUMERAL_MAP:
+        while num_int >= integer:
             result += numeral
-            n -= integer
+            num_int -= integer
     return result
 
-def from_roman(s):
+def from_roman(numeral_string):
     '''Convert Roman numeral to integer'''
     result = 0
     index = 0
     '''iterate through the map and match numeral string from largest
     to smallest, adding to the result once match
     '''
-    for numeral, integer in roman_numeral_map:
-        while s[index:index+len(numeral)] == numeral:
+    for numeral, integer in ROMAN_NUMERAL_MAP:
+        while numeral_string[index:index+len(numeral)] == numeral:
             result += integer
             index += len(numeral)
     return result
