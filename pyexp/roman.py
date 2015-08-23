68a8d329ba55c9d789788fed97c7cbf5cd2897ac
Cuong Nguyen <cuong.nguyentumanh@gmail.com>
2015-08-23 14:57:50 +0100

    Add bad input checking for from_roman - raise exception if needed

--- pyexp/roman.py
+++ pyexp/roman.py
@@ -1,11 +1,15 @@
 '''Module to convert to and from Roman numerals.
 Current range supported: 1-3999.
 '''
+import re
 
 class OutOfRangeError(ValueError):
     pass
 
 class NotIntegerError(ValueError):
+    pass
+
+class InvalidRomanNumeralError(ValueError):
     pass
 
 ROMAN_NUMERAL_MAP = (('M', 1000),
@@ -21,6 +25,18 @@ ROMAN_NUMERAL_MAP = (('M', 1000),
                      ('V', 5),
                      ('IV', 4),
                      ('I', 1))
+
+ROMAN_NUMERAL_PATTERN = re.compile('''
+    ^                         # beginning of string
+    M{0,3}                    # thousands - 0 to 3 Ms
+    (CM|CD|D?C{0,3})          # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 Cs),
+                              # or 500-800 (D, followed by 0 to 3 Cs)
+    (XC|XL|L?X{0,3})          # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 Xs),
+                              # or 50-80 (L, followed by 0 to 3 Xs)
+    (IX|IV|V?I{0,3})          # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 Is),
+                              # or 5-8 (V, followed by 0 to 3 Is)
+    $                         # end of string
+    ''', re.VERBOSE)
 
 
 def to_roman(num_int):
@@ -41,6 +57,9 @@ def to_roman(num_int):
 
 def from_roman(numeral_string):
     '''Convert Roman numeral to integer'''
+    if not ROMAN_NUMERAL_PATTERN.search(numeral_string):
+        raise InvalidRomanNumeralError(
+            'Invalid Roman numeral: {0}'.format(numeral_string))
     result = 0
     index = 0
     '''iterate through the map and match numeral string from largest
