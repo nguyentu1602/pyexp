24318c8ca4c96bb0646a0b6789db105fd3e04fab
Cuong Nguyen <cuong.nguyentumanh@gmail.com>
2015-09-02 22:05:10 +0100

    add a hash function for a permuted string

--- pyexp/string_permu_check.py
+++ pyexp/string_permu_check.py
@@ -4,4 +4,29 @@ of S is in a longer string L, assuming, of course, that
 permutations of S must be in contagious blocks in L.
 
 I will solve it in O(len(L)) time.
+For demonstration purpose, let's assume the characters
+are ASCII lowercase letters only.
 '''
+
+# Q: given a character, how to convert it into its
+# ASCII value? A: using ord() and chr()
+SHIFT = ord('a')
+
+def char_to_int(char):
+    '''convert char in a-z to 0-25
+    '''
+    # TODO: range check
+    return ord(char) - SHIFT
+
+def int_to_char(num):
+    '''convert num in 0-25 to a-z
+    '''
+    # TODO: range check
+    return chr(num + SHIFT)
+
+def string_permu_hash(in_str):
+    base = len(in_str) + 1
+    hash_result = 0
+    for c in in_str:
+        hash_result += base ** char_to_int(c)
+    return hash_result
