be2ac14fbb228e5a5addd393867b9b3c7267ba89
Cuong Nguyen <cuong.nguyentumanh@gmail.com>
2015-09-02 21:45:42 +0100

    Add and define string_permu_check problem.

---
+++ pyexp/string_permu_check.py
@@ -0,0 +1,7 @@
+'''Module to solve the algoritm question:
+Given a string S, how to count how many permutations
+of S is in a longer string L, assuming, of course, that
+permutations of S must be in contagious blocks in L.
+
+I will solve it in O(len(L)) time.
+'''
